b18ffe8250bc8a7f805d82a0873a437169ff2f0e
ludotielbeke <ludotielbeke@gmail.com>
2024-07-27 18:03:17 +0200

    added get endpoints for categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,8 +1,39 @@
+using api.Data;
+using api.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers;
 
+[Route("[Controller]")]
 public class CategoryController : ControllerBase
 {
+
+    private readonly AppDbContext _db;
     
+    public CategoryController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200)]
+    public IActionResult GetAll()
+    {
+        List<Category> categories = _db.Categories.ToList();
+        
+        return Ok(categories);
+    }
+
+    [HttpGet("id")]
+    public IActionResult GetOneItem(int id)
+    {
+        Category? category = _db.Categories.Find(id);
+
+        if (category == null)
+        {
+            return NotFound("category not found");
+        }
+        
+        return Ok(category);
+    }
 }
