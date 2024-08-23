9fed3605bf8022e0666ad2b2aed48c197ff0f332
ludo <ludotielbeke@gmail.com>
2024-08-23 21:08:37 +0200

    tets

--- SignalR/ScreenHub/ScreenHub.cs
+++ SignalR/ScreenHub/ScreenHub.cs
@@ -1,8 +1,12 @@
-﻿using api.Data;
+﻿using System.Text.Json;
+using api.Data;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.SignalR.ScreenHub;
 
+[DisableCors]
 public class ScreenHub : Hub<IScreenHub>
 {
 
@@ -12,27 +16,47 @@ public class ScreenHub : Hub<IScreenHub>
     {
         _db = db;
     }
+    
+    private Dictionary<string, int> clients = new Dictionary<string, int>();
 
     public override async Task OnConnectedAsync() {
         await Clients.All.RecieveMessage($"New Member ({Context.ConnectionId}): Joined");
+        
+        clients.Add(Context.ConnectionId, 0);
+        
         Console.WriteLine("hallo");
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        Console.WriteLine(Context.ConnectionId);
+
+        return Task.CompletedTask;
     }
 
     public async Task SendMessage(string message) {
         await Clients.All.RecieveMessage($"{Context.ConnectionId}: {message}");
     }
 
-    public async Task GetPostsFromScreenId(int screenId)
+    public async Task GetPostsFromScreenId()
     {
-        await Clients.Client(Context.ConnectionId).RecieveMessage($"Hoi {screenId}");
+        //id of the screen is gotton from the clients dictonary
+        //await Clients.Client(Context.ConnectionId).RecieveMessage();
+    }
+
+    public async Task GetAllPosts()
+    {
+        await Clients.Client(Context.ConnectionId).RecieveMessage(JsonSerializer.Serialize(_db.Posts.Include(x => x.Image).ToList()));
     }
 
     public async Task CheckIn(int screenId)
     {
-        
+        var test = clients.Where(x => x.Key == Context.ConnectionId).FirstOrDefault();
     }
 }
 
 public interface IScreenHub {
     Task RecieveMessage(string message);
+    
+    
 }
